476b665ae779a38fd18ec82c4fcbfe87108fa7c3
Kh.Moh3en <Mohsen.khorrami1990@gmail.com>
2025-01-03 16:30:40 +0100

    init Porototype

---
+++ Creational Patterns/Prototype/Prototype/IdInfo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
